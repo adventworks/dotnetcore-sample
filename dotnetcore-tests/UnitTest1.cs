d8bacc3e4fffd751bac9227bbc8c26fcc09f8b55
Mateo Escobedo <mat.esc@outlook.com>
2017-06-29 12:23:27 -0400

    Add test project

---
+++ dotnetcore-tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace dotnetcore_tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+        }
+    }
+}
